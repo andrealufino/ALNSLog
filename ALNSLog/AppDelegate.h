a5de74cf410532ae1335f5abcc55662fb2c455f3
Andrea Mario Lufino <andrea.lufino@me.com>
2015-03-01 22:45:07 +0100

    Initial Commit

---
+++ ALNSLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALNSLog
+//
+//  Created by Andrea Mario Lufino on 01/03/15.
+//  Copyright (c) 2015 Andrea Mario Lufino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALNSLog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ALNSLog
+//
+//  Created by Andrea Mario Lufino on 01/03/15.
+//  Copyright (c) 2015 Andrea Mario Lufino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
